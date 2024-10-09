251e6efecce56e52eb9960874a3a6c3483bc2529
ren <xiaodongrenpmo@gmail.com>
2024-10-09 17:01:39 +0000

    plano-curvedSurf lens

--- telecentric_lens.py
+++ telecentric_lens.py
@@ -37,27 +37,70 @@ c. constraints: to keep telecentricity of the optics
 import numpy as np
 import matplotlib.pyplot as plt
 #%%
+# 1. single surface lens
+''' 
+In the approprate paraxial limit, the hyperb and spheric
+sag of the surface becomes
+
+z ~ r^2/(2f[n-1])
+
+'''
 # hyperbole lens
-def One_surface_hyperb(f,n):
+def hyperbolic_plano_lens(f,n):
     e = n
     a = f/(1+e)
     c = a*e
     k = -e**2
     R = (c**2-a**2)/a
-
     zemax_lens = {'k': k,
                   'R': R,
                   'f': f,
                   'n': n}
     return zemax_lens
 # spheric lens
-def One_surface_sphere(f,n):
+def spherical_plano_lens(f,n):
     R = f*(n-1)
     zemax_lens = {'k': 0,
                   'R': R,
                   'f': f,
                   'n': n}
     return zemax_lens
+
+# plano-curvedsurf lens
+def plano_surf_lens(f,n,tc,
+                    D_size=10,
+                    poly_order = 10):
+    """
+    'tc' is the center thickness of the lens.
+    """
+    def sag(r):
+        #r2 = x**2 + y**2
+        r2 = r**2
+        f2 = f**2
+        b = f + (n-1)*tc - np.sqrt(f2 +r2)
+        a = n*np.sqrt(1+r2/(n**2*(f2+r2)-r2)) -1
+        return b/a
+    rho = np.linspace(0,1,50)
+    z   = sag(D_size/2*rho)
+    coeffi = np.polyfit(rho**2, z, poly_order)
+
+    zemax_lens = {'k': 'polynomial Surface',
+                  'R': '',
+                  'sag': sag,
+                  'coeff': coeffi,
+                  'Normalization': D_size/2,
+                  'f': f,
+                  'n': n}
+    return zemax_lens
+
+# Double surface lens
+def Double_surf_lens(f,n):
+    pass
+
+
+# 2.  calculate the required focal length of the 
+# two-lens system, with constraint of separation distance
+# of the lenses is equial to f2
 
 def Dual_lens(f_eff, L):
     f2 = L
@@ -67,8 +110,22 @@ def Dual_lens(f_eff, L):
 #%%    
 index = 3.416
 f = Dual_lens(640, 800)
-lens1 = One_surface_hyperb(f[0],index)
-lens2 = One_surface_hyperb(f[1],index)
+lens1 = hyperbolic_plano_lens(f[0],index)
+lens2 = hyperbolic_plano_lens(f[1],index)
 print(lens1,'\n',lens2)
 # %%
 
+lens3 = plano_surf_lens(f[0],index,1,D_size=400,poly_order = 20)
+# %%
+lens3
+# %%
+x = np.linspace(-1,1,101)
+Sag = np.poly1d(lens3['coeff'])
+y = Sag(x**2)
+
+# %%
+plt.figure(figsize=(5,3))
+plt.plot(y,x*50)
+plt.axis('equal')
+plt.show()
+# %%
